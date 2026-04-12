ce2b6a6fdaa3898aa1a2ae53dc1217d99b76670f
rut-alt <rut.18.gonzalez@gmail.com>
2026-04-12 22:26:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def obtener_evento(step):
         {"texto": "Algo interrumpe, Marta necesita que vayas a dar de comer a los gatos.", "ok": "Decirle que no tienes tiempo", "bad": "Ir a dar de comer a los gatos"},
         {"texto": "Lorena te llama para que la recojas.", "ok": "Que se pida un Uber", "bad": "Ir a recogerla."},
         {"texto": "Ves a alguien familiar en una cafetería.", "ok": "Ignorar", "bad": "Entrar a mirar quién es."},
-        {"texto": "Te cruzas con alguien.", "ok": "Seguir", "bad": "Pararte"},
+        {"texto": "Llamada de Julia, que querrá?", "ok": "Cogérlo", "bad": "Son eternas, ni loca."},
         {"texto": "Último momento, llamada de Andrea.", "ok": "Colgar, no es tan urgente", "bad": "Ver qué quiere."},
     ]
     return eventos[step]
