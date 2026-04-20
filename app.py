4d11cd58356baf281df48433a2d0953023e487af
rut-alt <rut.18.gonzalez@gmail.com>
2026-04-20 15:10:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -298,7 +298,7 @@ elif st.session_state.pantalla == "win":
     Tipo de novia: **{tipo}**
     Aquí tienes vuestro resumen.
     NO CIERRES ESTA PANTALLA.
-    ESPERA QUE CARGUE Y DISFRUTA!
+    ACTIVA EL SONIDO, ESPERA QUE CARGUE Y DISFRUTA!
     """)
     st.markdown('</div>', unsafe_allow_html=True)
 
